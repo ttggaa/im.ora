a4b681a51669016b0e5e5d08e5befce6e91b2852
smpower <rf.wangchn@gmail.com>
2018-11-22 15:17:38 +0800

    编写 Footer 组件

--- src/components/footer/views/footer.js
+++ src/components/footer/views/footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import logoImage from './logo.png';
 import './style.css';
 
 const Footer = () => (
   <div className="footer">
-    <div className="footer-info">
-      <span>咨询热线: 4000-130-520</span>
-      <span>Copyright©2018 北京小鱼果果科技有限公司</span>
-      <span>京ICP备18050673号-1</span>
+    <p className="footer-wechat text-center">
+      <span>微信公众号: jzqinggan</span>
+    </p>
+    <p className="footer-phone text-center">热线电话: 4006-299-520</p>
+    <div className="footer-logo">
+      <img src={logoImage} alt="橘子情感 LOGO" />
     </div>
+    <p className="footer-company text-center">青岛橘子创想文化传播邮箱公司</p>
+    <p className="footer-address text-center">地址: 青岛市市南区银川西路动漫产业园A做212B</p>
+    <p className="footer-mark text-center">备案号: 鲁 ICP 备 17023741 号</p>
   </div>
 );
 
