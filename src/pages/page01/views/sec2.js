405eea2d14a16dd00e6a724cab9d4082dae48893
smpower <rf.wangchn@gmail.com>
2018-11-23 15:36:18 +0800

    编写挽回爱情页面【挽回失败原因】模块结构和样式

---
+++ src/pages/page01/views/sec2.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Sec2 extends Component {
+  render() {
+    return (
+      <div className="page01-sec2">
+        Sec2 Component
+      </div>
+    );
+  }
+}
+
+export default Sec2;
---
+++ src/pages/page01/views/sec3.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Sec3 extends Component {
+  render() {
+    return (
+      <div className="page01-sec3">
+        Sec3 Component
+      </div>
+    );
+  }
+}
+
+export default Sec3;
