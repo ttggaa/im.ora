841055fe3b37751c331cac189447620a4b8c05b6
smpower <rf.wangchn@gmail.com>
2018-11-25 09:55:34 +0800

    编写挽回爱情页面【擅长挽回的爱情类型】模块静态结构和样式

--- src/pages/page01/views/sec2.js
+++ src/pages/page01/views/sec2.js
@@ -3,8 +3,38 @@ import React, {Component} from 'react';
 class Sec2 extends Component {
   render() {
     return (
-      <div className="page01-sec2">
-        Sec2 Component
+      <div className="page01-sec02">
+	<h1>擅长挽回的爱情类型</h1>
+	<div className="page01-sec02-content">
+	  <div className="page01-sec02-group">
+	    <div className="page01-sec02-group-item">
+	      <i></i>
+	      <h3 className="title">性格不合</h3>
+	      <span>无法沟通</span>
+	      <span>互相误解、冷战</span>
+	    </div>
+	    <div className="page01-sec02-group-item">
+	      <i></i>
+	      <h3 className="title">性格不合</h3>
+	      <span>无法沟通</span>
+	      <span>互相误解、冷战</span>
+	    </div>
+	  </div>
+	  <div className="page01-sec02-group">
+	    <div className="page01-sec02-group-item">
+	      <i></i>
+	      <h3 className="title">性格不合</h3>
+	      <span>无法沟通</span>
+	      <span>互相误解、冷战</span>
+	    </div>
+	    <div className="page01-sec02-group-item">
+	      <i></i>
+	      <h3 className="title">性格不合</h3>
+	      <span>无法沟通</span>
+	      <span>互相误解、冷战</span>
+	    </div>
+	  </div>
+	</div>
       </div>
     );
   }
