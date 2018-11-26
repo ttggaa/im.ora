bb215f98726a9d94809a7dc76ac2f07d6df10710
smpower <rf.wangchn@gmail.com>
2018-11-26 09:15:12 +0800

    初始化挽救婚姻页面

--- src/pages/page02/views/page02.js
+++ src/pages/page02/views/page02.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {view as Header} from '../../../components/header/';
 import {view as Footer} from '../../../components/footer/';
-import Service from './service.js';
-import Circuit from './circuit.js';
-import Guarantee from './guarantee.js';
 
 import toPc from '../../../utilities/toPc';
 
@@ -11,17 +8,20 @@ import './style.css';
 
 import bannerImage from './images/banner.png';
 
-toPc();
+class Page02 extends React.Component {
+  componentWillMount() {
+    toPc();
+  }
 
-const Page02 = () => (
-  <div className="page02">
-    <Header home={false} title="服务范围"/>
-    <img className="page02-banner" src={bannerImage} alt="服务范围_权威专家" />
-    <Service />
-    <Circuit />
-    <Guarantee />
-    <Footer />
-  </div>
-);
+  render() {
+    return (
+      <div className="page02">
+	<Header home={false} title="服务范围"/>
+	<img className="page02-banner" src={bannerImage} alt="服务范围_权威专家" />
+	<Footer />
+      </div>
+    )
+  }
+};
 
 export default Page02;
